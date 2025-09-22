04e4973586bbacb1239104f68e5f8c16a941c761
VnszinN <vinicius.saidy@gmail.com>
2025-09-22 14:53:33 -0300

    style: adjust header responsiveness

--- FrontEnd/src/components/header/Header.jsx
+++ FrontEnd/src/components/header/Header.jsx
@@ -1,48 +1,107 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { FiUser, FiPlus } from "react-icons/fi";
 import logo from "../../assets/logoCefet.svg";
 import style from "./Header.module.scss";
 import { NavLink } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const [windowWidth, setWidth] = useState(window.innerWidth);
+  const route = useLocation();
 
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
+  const isHome = route.pathname == "/";
+  const isMatches = route.pathname == "/matches";
+  const isChat = route.pathname == "/chats";
+  const isItens = route.pathname == "/itens";
 
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  if (windowWidth > 600) {
-    return (
-      <>
-        <nav id="nav">
-          {" "}
-          <NavLink> OIIIIII</NavLink>
-          <button onClick={() => console.log(windowWidth)}></button>
-        </nav>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className={style.body_header}>
-          <section className={style.left_section_header}>
-            <img className={style.logo_header} src={logo} alt="" />
-            <span className={style.tittle_header}>A&P</span>
-          </section>
-          <section className={style.right_section_header}>
-            <FiPlus className={style.plus_header} />
-            <FiUser className={style.user_header} />
-          </section>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className={style.body_header}>
+        <section className={style.left_section_header}>
+          <img className={style.logo_header} src={logo} alt="" />
+          <span className={style.tittle_header}>A&P</span>
+        </section>
+        <section className={style.mid_section_header}>
+          <div
+            className={
+              isHome
+                ? `${style.box_nav_header} ${style.box_nav_header_current}`
+                : style.box_nav_header
+            }
+          >
+            <NavLink
+              to="/"
+              className={
+                isHome
+                  ? `${style.nav_header} ${style.nav_header_current}`
+                  : style.nav_header
+              }
+            >
+              Home
+            </NavLink>
+          </div>
+          <div
+            className={
+              isMatches
+                ? `${style.box_nav_header} ${style.box_nav_header_current}`
+                : style.box_nav_header
+            }
+          >
+            <NavLink
+              to="/matches"
+              className={
+                isMatches
+                  ? `${style.nav_header} ${style.nav_header_current}`
+                  : style.nav_header
+              }
+            >
+              Matches
+            </NavLink>
+          </div>
+          <div
+            className={
+              isChat
+                ? `${style.box_nav_header} ${style.box_nav_header_current}`
+                : style.box_nav_header
+            }
+          >
+            <NavLink
+              to="/chats"
+              className={
+                isChat
+                  ? `${style.nav_header} ${style.nav_header_current}`
+                  : style.nav_header
+              }
+            >
+              Chats
+            </NavLink>
+          </div>
+          <div
+            className={
+              isItens
+                ? `${style.box_nav_header} ${style.box_nav_header_current}`
+                : style.box_nav_header
+            }
+          >
+            <NavLink
+              to="/itens"
+              className={
+                isItens
+                  ? `${style.nav_header} ${style.nav_header_current}`
+                  : style.nav_header
+              }
+            >
+              Itens
+            </NavLink>
+          </div>
+        </section>
+        <section className={style.right_section_header}>
+          <FiPlus className={style.plus_header} />
+          <FiUser className={style.user_header} />
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Header;
