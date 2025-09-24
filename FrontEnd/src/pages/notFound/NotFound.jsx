e99dadf2acfb18e95f137fb6c205d0965489c22b
VnszinN <vinicius.saidy@gmail.com>
2025-09-24 00:28:57 -0300

    style: replace box emoji with box.svg[D

--- FrontEnd/src/pages/notFound/NotFound.jsx
+++ FrontEnd/src/pages/notFound/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./NotFound.module.scss";
+import box from "../../assets/box.svg";
 
 const NotFound = () => {
   return (
     <div className={style.not_found_container}>
-      <span>📦</span>
+      <img src={box} />
       <div className={style.shadow} />
       <div className={style.text_container}>
         <h1>404</h1>
