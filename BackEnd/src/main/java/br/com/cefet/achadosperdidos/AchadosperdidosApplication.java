ab16279b75ca5494949fdc7e2cfdb1f6caea7b18
caiosantosdev <caiosantosdev@gmail.com>
2025-09-25 16:56:00 -0300

    feat: added docker compose and backend

---
+++ BackEnd/src/main/java/br/com/cefet/achadosperdidos/AchadosperdidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.cefet.achadosperdidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchadosperdidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchadosperdidosApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/br/com/cefet/achadosperdidos/AchadosperdidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cefet.achadosperdidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class AchadosperdidosApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
