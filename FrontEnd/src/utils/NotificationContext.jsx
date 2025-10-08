06bda2a7f7c5acdec77abe172a335c53378a41f5
VnszinN <vinicius.saidy@gmail.com>
2025-10-08 12:30:29 -0300

    style: apply slide animation and filled style to notification box

--- FrontEnd/src/utils/NotificationContext.jsx
+++ FrontEnd/src/utils/NotificationContext.jsx
@@ -1,41 +1,51 @@
-import React, { createContext, useContext, useState } from 'react';
-import { Snackbar, Alert } from '@mui/material';
+import React, { createContext, useContext, useState } from "react";
+import { Snackbar, Alert, Slide } from "@mui/material";
 
 // Criação do Contexto de Notificação
 const NotificationContext = createContext();
 
+const SlideTransition = (props) => {
+  return <Slide {...props} direction="left" />;
+};
 // Provider de Notificação
 export const NotificationProvider = ({ children }) => {
-    const [open, setOpen] = useState(false);       // Controle de exibição do Snackbar
-    const [message, setMessage] = useState('');    // Mensagem da notificação
-    const [severity, setSeverity] = useState(''); // Tipo de severidade (info, success, error, etc.)
+  const [open, setOpen] = useState(false); // Controle de exibição do Snackbar
+  const [message, setMessage] = useState(""); // Mensagem da notificação
+  const [severity, setSeverity] = useState(""); // Tipo de severidade (info, success, error, etc.)
 
-    const showNotification = (msg, severity) => {
-        setMessage(msg);
-        setSeverity(severity);
-        setOpen(true);
-    };
+  const showNotification = (msg, severity) => {
+    setMessage(msg);
+    setSeverity(severity);
+    setOpen(true);
+  };
 
-    const closeNotification = () => {
-        setOpen(false);
-    };
+  const closeNotification = () => {
+    setOpen(false);
+  };
 
-    return (
-        <NotificationContext.Provider value={{ showNotification }}>
-            {/* Snackbar para exibir notificações */}
-            <Snackbar
-                open={open}
-                autoHideDuration={2000} // Duração antes de desaparecer
-                onClose={closeNotification}
-                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            >
-                <Alert onClose={closeNotification} severity={severity} sx={{ width: '100%' }}>
-                    {message}
-                </Alert>
-            </Snackbar>
-            {children}
-        </NotificationContext.Provider>
-    );
+  return (
+    <NotificationContext.Provider value={{ showNotification }}>
+      {/* Snackbar para exibir notificações */}
+      <Snackbar
+        open={open}
+        autoHideDuration={3000} // Duração antes de desaparecer
+        onClose={closeNotification}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        TransitionComponent={SlideTransition}
+        key={SlideTransition.name}
+      >
+        <Alert
+          onClose={closeNotification}
+          variant="filled"
+          severity={severity}
+          sx={{ width: "60%" }}
+        >
+          {message}
+        </Alert>
+      </Snackbar>
+      {children}
+    </NotificationContext.Provider>
+  );
 };
 
 // Hook para usar as notificações
