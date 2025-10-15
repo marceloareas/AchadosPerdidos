684c2d15fcd2f00fcad24b7eaa454b9877dbca79
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2025-10-15 16:18:33 -0300

    feat: add endpoint to retrieve authenticated user and create AuthenticatedUserDTO

--- BackEnd/src/main/java/br/com/cefet/achadosperdidos/controllers/AuthController.java
+++ BackEnd/src/main/java/br/com/cefet/achadosperdidos/controllers/AuthController.java
@@ -6,13 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cefet.achadosperdidos.services.UsuarioService;
 import br.com.cefet.achadosperdidos.dto.auth.AuthenticatedDTO;
+import br.com.cefet.achadosperdidos.dto.auth.AuthenticatedUserDTO;
 import br.com.cefet.achadosperdidos.dto.auth.LoginDTO;
 import br.com.cefet.achadosperdidos.dto.usuario.UsuarioRequestDTO;
 import br.com.cefet.achadosperdidos.dto.usuario.UsuarioResponseDTO;
@@ -29,22 +32,30 @@ public class AuthController {
     private UsuarioService usuarioService;
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticatedDTO> login(@RequestBody LoginDTO loginDTO){
+    public ResponseEntity<AuthenticatedDTO> login(@RequestBody LoginDTO loginDTO) {
         AuthenticatedDTO auth = authService.login(loginDTO.getEmail(), loginDTO.getSenha());
         return ResponseEntity.ok(auth);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody UsuarioRequestDTO usuarioRequestDTO){
+    public ResponseEntity<String> register(@RequestBody UsuarioRequestDTO usuarioRequestDTO) {
         UsuarioResponseDTO usuarioResponseDTO = usuarioService.create(usuarioRequestDTO);
         return ResponseEntity.ok("Usuário registrado com sucesso!");
     }
 
     @PostMapping("/updatePassword")
-    public ResponseEntity<String> updatePassword(@RequestBody UpdatePasswordRequestDTO updatePasswordRequestDTO){
+    public ResponseEntity<String> updatePassword(@RequestBody UpdatePasswordRequestDTO updatePasswordRequestDTO) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Usuario usuario = (Usuario)auth.getPrincipal();
-        String resposta = usuarioService.updatePassword(usuario.getId(), updatePasswordRequestDTO.getSenha(), updatePasswordRequestDTO.getConfirmacaoSenha());
+        Usuario usuario = (Usuario) auth.getPrincipal();
+        String resposta = usuarioService.updatePassword(usuario.getId(), updatePasswordRequestDTO.getSenha(),
+                updatePasswordRequestDTO.getConfirmacaoSenha());
         return ResponseEntity.ok(resposta);
+    }
+
+     @GetMapping("/me")
+    public ResponseEntity<AuthenticatedUserDTO> getAuthenticatedUser(
+            @RequestHeader("Authorization") String token) {
+        AuthenticatedUserDTO user = authService.getAuthenticatedUser(token);
+        return ResponseEntity.ok(user);
     }
 }
