c24c48bf20a5a3a8eb0c006e751da11ec2559abf
VnszinN <vinicius.saidy@gmail.com>
2025-10-15 14:08:38 -0300

    feat: create and integrate myItems Page

--- FrontEnd/src/pages/meusItens/MeusItens.jsx
+++ FrontEnd/src/pages/meusItens/MeusItens.jsx
@@ -1,10 +1,96 @@
-import React from "react";
+import React, { useEffect, useInsertionEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
+import CustomButton from "../../components/ui/button/CustomButton";
+import ItemCard from "../../components/ui/itemCard/ItemCard";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "../../components/ui/tabs/Tab.jsx";
 
+import style from "./MeusItens.module.scss";
+import useItemStore from "../../store/item.js";
 const MeusItens = () => {
+  const [activeTab, setActiveTab] = useState("all");
+
+  const { itemsUser, getUserItens } = useItemStore();
+  // Mock data for user's items
+  useEffect(() => {
+    getUserItens();
+  }, []);
+
+  const filteredItems = itemsUser.filter((item) => {
+    if (activeTab === "all") return true;
+    return item.tipo === activeTab;
+  });
+
   return (
     <Layout>
-      <h1>MEUS ITENS</h1>
+      <section className={style.titleMyItems}>
+        <div className={style.countMyItems}>
+          <h1>Meus Itens</h1>
+          <p>
+            {itemsUser.length}{" "}
+            {itemsUser.length === 1 ? "item cadastrado" : "itens cadastrados"}
+          </p>
+        </div>
+      </section>
+      <section className={style.filtro}>
+        <Tabs
+          value={activeTab}
+          onValueChange={setActiveTab}
+          className={style.tabs_container}
+        >
+          <TabsList className={style.tabs_list}>
+            <TabsTrigger value="all">Todos ({itemsUser.length})</TabsTrigger>
+            <TabsTrigger value="PERDIDO">
+              Perdidos ({itemsUser.filter((i) => i.tipo == "PERDIDO").length})
+            </TabsTrigger>
+            <TabsTrigger value="ACHADO">
+              Encontrados ({itemsUser.filter((i) => i.tipo == "ACHADO").length})
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value={activeTab} className={style.tabs_content}>
+            {filteredItems.length === 0 ? (
+              <div className={style.empty_state}>
+                <div className={style.empty_icon}>
+                  <Plus className={style.icon} />
+                </div>
+                <h3 className={style.empty_title}>Nenhum item encontrado</h3>
+                <p className={style.empty_subtitle}>
+                  {activeTab === "all"
+                    ? "Você ainda não cadastrou nenhum item"
+                    : `Você não tem itens ${
+                        activeTab === "PERDIDO" ? "perdidos" : "encontrados"
+                      }`}
+                </p>
+                <CustomButton asChild>Cadastrar Item</CustomButton>
+              </div>
+            ) : (
+              <div className={style.items_list}>
+                {filteredItems.map((item) => {
+                  const isLost = item.tipo === "PERDIDO";
+
+                  return (
+                    <ItemCard
+                      key={item.id}
+                      itemType={isLost}
+                      onClick={() => handleCardClick(item)}
+                      title={item.nome}
+                      description={item.descricao}
+                      location={item.localizacao}
+                      category={item.categorias[0].nome}
+                      showOptions={true}
+                    />
+                  );
+                })}
+              </div>
+            )}
+          </TabsContent>
+        </Tabs>
+      </section>
     </Layout>
   );
 };
