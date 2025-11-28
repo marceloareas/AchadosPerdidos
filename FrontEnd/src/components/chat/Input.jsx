c487a84154fc89339de56f8761f44ee84b6ea7bd
VnszinN <vinicius.saidy@gmail.com>
2025-11-27 23:40:45 -0300

    style: improve input message style

--- FrontEnd/src/components/chat/Input.jsx
+++ FrontEnd/src/components/chat/Input.jsx
@@ -3,12 +3,6 @@ import style from "./Chat.module.scss";
 import { FaPaperPlane, FaFileImage } from "react-icons/fa";
 
 const Input = () => {
-  // const area = document.querySelector(".inputMessage");
-
-  // area.addEventListener("input", () => {
-  //   area.style.height = "auto"; // reseta
-  //   area.style.height = area.scrollHeight + "px"; // ajusta
-  // });
   const textRef = useRef(null);
 
   useEffect(() => {
@@ -25,16 +19,18 @@ const Input = () => {
     return () => el.removeEventListener("input", resize);
   }, []);
   return (
-    <div className={style.wrapInputChat}>
-      <button className={style.iconSendMessage}>
-        <FaFileImage />
-      </button>
-      <textarea
-        ref={textRef}
-        rows={1}
-        placeholder="Mensagem..."
-        className={style.inputMessage}
-      />
+    <div className={style.contentInput}>
+      <div className={style.wrapInputChat}>
+        <button className={style.iconImage}>
+          <FaFileImage />
+        </button>
+        <textarea
+          ref={textRef}
+          rows={1}
+          placeholder="Mensagem..."
+          className={style.inputMessage}
+        />
+      </div>
       <button className={style.iconSendMessage}>
         <FaPaperPlane />
       </button>
