c5efff1c378f646b99847a4a7da271db984e1440
VnszinN <vinicius.saidy@gmail.com>
2025-11-27 21:34:17 -0300

    style: adjust display on Matches Pages

--- FrontEnd/src/pages/matchesArquivados/MatchesArquivados.jsx
+++ FrontEnd/src/pages/matchesArquivados/MatchesArquivados.jsx
@@ -4,9 +4,12 @@ import MatchCard from "../../components/ui/matchCard/MatchCard";
 import style from "./MatchesArquivados.module.scss";
 import Layout from "../../components/layout/Layout";
 import useMatchStore from "../../store/match";
+import { IoReturnUpBackOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const MatchesArquivados = () => {
   const { matchesArquivados, getMatchesArquivados } = useMatchStore();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getMatchesArquivados();
@@ -16,6 +19,13 @@ const MatchesArquivados = () => {
     <Layout>
       <Box className={style.matchesPage}>
         <Box className={style.header}>
+          <div className={style.iconBackMatches}>
+            <IoReturnUpBackOutline
+              className={style.back_matches}
+              size={45}
+              onClick={() => navigate("/matches")}
+            />
+          </div>
           <Typography variant="h4" component="h1" gutterBottom>
             Matches arquivados
           </Typography>
@@ -29,7 +39,9 @@ const MatchesArquivados = () => {
           ) : (
             Array.isArray(matchesArquivados) &&
             matchesArquivados.length > 0 &&
-            matchesArquivados.map((match) => <MatchCard key={match.id} match={match} />)
+            matchesArquivados.map((match) => (
+              <MatchCard key={match.id} match={match} />
+            ))
           )}
         </Box>
       </Box>
