6071eed27ad914854ce2fdc7a138ab16d3fb32bc
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2025-12-15 01:13:55 -0300

    refactor: consulta de matches ativos

--- BackEnd/src/main/java/br/com/cefet/achadosperdidos/repositories/MatchRepository.java
+++ BackEnd/src/main/java/br/com/cefet/achadosperdidos/repositories/MatchRepository.java
@@ -11,15 +11,26 @@ import java.util.Optional;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
     Optional<Match> findByItemPerdido_IdAndItemAchado_Id(Long perdidoId, Long achadoId);
+
     List<Match> findByItemPerdido_Id(Long perdidoId);
+
     List<Match> findByItemAchado_Id(Long achadoId);
 
     @Query("SELECT m FROM Match m WHERE m.itemAchado.usuario.id = :userId OR m.itemPerdido.usuario.id = :userId")
     List<Match> findAllByUsuarioId(@Param("userId") Long userId);
 
-    @Query("SELECT m FROM Match m WHERE (m.itemPerdido.usuario.id = :userId AND m.arquivadoPorItemPerdido = false) OR (m.itemAchado.usuario.id = :userId AND m.arquivadoPorItemAchado = false)")
-    List<Match> findAllActiveByUsuarioId(@Param("userId") Long userId);         
-    
+    @Query("""
+            SELECT m FROM Match m
+            WHERE m.isFinalizado = false
+            AND (
+               (m.itemPerdido.usuario.id = :userId AND m.arquivadoPorItemPerdido = false)
+               OR
+               (m.itemAchado.usuario.id = :userId AND m.arquivadoPorItemAchado = false)
+            )
+            """)
+
+    List<Match> findAllActiveByUsuarioId(@Param("userId") Long userId);
+
     @Query("SELECT m FROM Match m WHERE (m.itemPerdido.usuario.id = :userId AND m.arquivadoPorItemPerdido = true) OR (m.itemAchado.usuario.id = :userId AND m.arquivadoPorItemAchado = true)")
     List<Match> findAllArchivedByUsuarioId(@Param("userId") Long userId);
 }
